bea9044d9ba6520c9b2401db1490616499ef95e2
Pavlo Brychka <brychkapavlo.work@gmail.com>
2023-10-19 16:07:31 +0300

    fix error

--- backendAPI/Data/Entities/BasketEntity.cs
+++ backendAPI/Data/Entities/BasketEntity.cs
@@ -15,25 +15,25 @@ namespace backendAPI.Data.Entities
         /// Користувач
         /// </summary>
         [ForeignKey("User")]
-<<<<<<< HEAD
+
         public int? UserId { get; set; }
-=======
-        public int UserId { get; set; }
->>>>>>> f0834f38439bff0e5bd32e05f7d5e1b133f7b843
+
+       
+
 
         /// <summary>
         /// Продукт
         /// </summary>
         [ForeignKey("Product")]
-<<<<<<< HEAD
+
         public int? ProductId { get; set; }
 
         public virtual UserEntity? User { get; set; }
-=======
-        public int ProductId { get; set; }
 
-        public virtual UserEntity User { get; set; }
->>>>>>> f0834f38439bff0e5bd32e05f7d5e1b133f7b843
+       
+
+      
+
         //public virtual ProductEntity Product { get; set; }
     }
 }
